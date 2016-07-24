6f708e06042a61912b3ab03c2c874cb591114af1
Kureev Alexey <kureev-mail@ya.ru>
2016-07-24 23:34:40 +0200

    Add weak against and strong against

--- src/components/StrongAgainstList.js
+++ src/components/StrongAgainstList.js
@@ -1,7 +1,9 @@
 /* @flow */
 
 import React, { PropTypes, Component } from 'react';
+import { InteractionManager } from 'react-native';
 import PokemonList from './PokemonList';
+import db from '../db';
 
 type Props = {
   onNavigate: Function;
@@ -11,11 +13,37 @@ export default class StrongAgainstList extends Component<void, Props, void> {
 
   static propTypes = {
     onNavigate: PropTypes.func.isRequired,
+    pokemon: PropTypes.object.isRequired,
   };
 
+  constructor(props, ctx) {
+    super(props, ctx);
+    this.state = {
+      pokemons: [],
+    };
+  }
+
   render() {
+    if (!this.state.pokemons.length) {
+      InteractionManager.runAfterInteractions(() => {
+        let pokemons = [];
+        this.props.pokemon.type.forEach(type => {
+          db.objects('Pokemon').forEach(p => {
+            p.type.forEach(t => {
+              t.weaknesses.forEach(s => {
+                if (s.name === type.name && !pokemons.includes(p)) {
+                  pokemons.push(p);
+                }
+              });
+            });
+          });
+        });
+        this.setState({pokemons});
+      });
+    }
+
     return (
-      <PokemonList data={[]} onNavigate={this.props.onNavigate} />
+      <PokemonList data={this.state.pokemons} onNavigate={this.props.onNavigate} />
     );
   }
 }
