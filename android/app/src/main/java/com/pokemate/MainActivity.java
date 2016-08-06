19099625cfb3e4a2f70494f297ebb0a9c1ad1b3e
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-08-06 07:37:13 +0530

    Rename project to 'Pokemate' (#6)

--- android/app/src/main/java/com/pokemate/MainActivity.java
+++ android/app/src/main/java/com/pokemate/MainActivity.java
@@ -1,4 +1,4 @@
-package com.pokematch;
+package com.pokemate;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "pokematch";
+        return "Pokemate";
     }
 }
--- android/app/src/main/java/com/pokemate/MainApplication.java
+++ android/app/src/main/java/com/pokemate/MainApplication.java
@@ -1,4 +1,4 @@
-package com.pokematch;
+package com.pokemate;
 
 import android.app.Application;
 import android.util.Log;
