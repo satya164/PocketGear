5ce0ca0c314a7510c05bc87c12e6b24876f942e6
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-08-06 07:36:01 +0530

    Rename project to 'Pokemate'

--- ios/PokemateTests/PokemateTests.m
+++ ios/PokemateTests/PokemateTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface pokematchTests : XCTestCase
+@interface PokemateTests : XCTestCase
 
 @end
 
-@implementation pokematchTests
+@implementation PokemateTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
