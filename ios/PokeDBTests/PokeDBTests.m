ed9add9e31489ebceaf8b1b2c2936e56bf1bce96
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-09-01 23:19:14 +0530

    Rename app to PokeDB

--- ios/PokeDBTests/PokeDBTests.m
+++ ios/PokeDBTests/PokeDBTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface MewDBTests : XCTestCase
+@interface PokeDBTests : XCTestCase
 
 @end
 
-@implementation MewDBTests
+@implementation PokeDBTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
