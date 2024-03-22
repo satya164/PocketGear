680d63fb12fc96fee2e9fcb0d004ad8c5b32f689
Satyajit Sahoo <satyajit.happy@gmail.com>
2024-03-22 15:58:22 +0100

    refactor: use expo-splash-screen

--- App.tsx
+++ App.tsx
@@ -1,15 +1,42 @@
-import 'react-native-gesture-handler';
 import { MaterialIcons } from '@expo/vector-icons';
-import {
-  NavigationContainer,
-  useNavigationContainerRef,
-} from '@react-navigation/native';
-import AppLoading from 'expo-app-loading';
+import { NavigationContainer } from '@react-navigation/native';
 import * as Font from 'expo-font';
-import React from 'react';
-import { Platform, StyleSheet, View } from 'react-native';
-import Home from './src/components/Home';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
+import React, { useEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
+import 'react-native-gesture-handler';
+import Home from './src/components/Home';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+  const [fontsLoaded, setFontsLoaded] = React.useState(false);
+
+  useEffect(() => {
+    Font.loadAsync({
+      'Montserrat': require('./assets/fonts/Montserrat.otf'),
+      'Montserrat-SemiBold': require('./assets/fonts/Montserrat-SemiBold.otf'),
+      ...MaterialIcons.font,
+    })
+      .finally(() => setFontsLoaded(true))
+      .catch(() => {});
+  }, []);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <NavigationContainer onReady={() => SplashScreen.hideAsync()}>
+      <StatusBar translucent />
+      <View style={styles.container}>
+        <View style={styles.statusbar} />
+        <Home />
+      </View>
+    </NavigationContainer>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -19,30 +46,3 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
 });
-
-export default function App() {
-  const [isReady, setIsReady] = React.useState(false);
-  const ref = useNavigationContainerRef();
-
-  React.useEffect(() => {
-    Font.loadAsync({
-      'Montserrat': require('./assets/fonts/Montserrat.otf'),
-      'Montserrat-SemiBold': require('./assets/fonts/Montserrat-SemiBold.otf'),
-      ...MaterialIcons.font,
-    }).then(() => setIsReady(true));
-  }, []);
-
-  if (!isReady) {
-    return <AppLoading />;
-  }
-
-  return (
-    <NavigationContainer ref={ref}>
-      <StatusBar translucent />
-      <View style={styles.container}>
-        <View style={styles.statusbar} />
-        <Home />
-      </View>
-    </NavigationContainer>
-  );
-}
